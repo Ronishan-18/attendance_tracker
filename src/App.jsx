30679000396b937d68d186435e2f4033e8405e62
Ronishan-18 <ronisonroni0@gmail.com>
2025-12-31 14:39:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard'
+import AttendancePage from './pages/attendencepage'
+import NotFound from './pages/notfound'
+
+function App() {
+  return (
+    <Router basename="/Student_Attendance">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/attendance" element={<AttendancePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/attendencepage.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Header from "../components/header";
+import AttendanceTable from "../components/attendencetable";
+import Form from "../components/attendenceform";
+const initialStudents = [
+  { id: 1, roll: "001", name: "Alice", status: "absent", remarks: "" },
+  { id: 2, roll: "002", name: "Bob", status: "present", remarks: "" },
+  { id: 3, roll: "003", name: "Charlie", status: "absent", remarks: "" },
+];
+function AttendancePage() {
+  const [students, setStudents] = useState(initialStudents);
+  const [search, setSearch] = useState("");
+  const toggleStatus = (id) => {
+    setStudents(
+      students.map((student) =>
+        student.id === id
+          ? {
+              ...student,
+              status: student.status === "present" ? "absent" : "present",
+            }
+          : student
+      )
+    );
+  };
+  const updateRemarks = (id, text) => {
+    setStudents(
+      students.map((student) =>
+        student.id === id ? { ...student, remarks: text } : student
+      )
+    );
+  };
+  const filteredStudents = students.filter((student) =>
+    student.name.toLowerCase().includes(search.toLowerCase())
+  );
+  const presentCount = students.filter(s => s.status === "present").length;
+  const absentCount = students.filter(s => s.status === "absent").length;
+  return (
+    <div className="p-4">
+      <Header />
+    
+      <AttendanceTable
+        students={filteredStudents}
+        toggleStatus={toggleStatus}
+        updateRemarks={updateRemarks}
+      />
+        <Form
+        search={search}
+        setSearch={setSearch}
+        present={presentCount}
+        absent={absentCount}
+        total={students.length}
+      />
+    </div>
+  );
+}
+export default AttendancePage;
